f2c53fe145fc78e2e87e135577a2e3f4e05d78d4
wzhang <william.zhang@cbsinteractive.com>
2018-07-18 16:10:28 -0700

    update compiled

--- ethereum/ProjectManager.js
+++ ethereum/ProjectManager.js
@@ -3,7 +3,7 @@ import ProjectManager from './build/contracts/ProjectManager.json';
 
 const instance = new web3.eth.Contract(
   ProjectManager.abi,
-  '0xdbf953177d9b0bf6973844e31ea0471160728c76'
+  '0x23aA303Ea9682B65b94AD6E5E2f49EE2DDE28C0D'
 );
 
 export default instance;	
